559c83f25f16dc4aeb1780b91d8147d4cbd2e2f8
Avi Weit <weit@il.ibm.com>
2021-04-22 09:16:59 +0300

    Calls to initRAN() only.

--- gnb-gw/main.go
+++ gnb-gw/main.go
@@ -2,59 +2,61 @@ package main
 
 import (
 	"log"
-	"os"
-	"os/signal"
-	"context"
-	"syscall"
+//	"os"
+//	"os/signal"
+//	"context"
+//	"syscall"
 )
 
 func main() {
 	log.SetPrefix("[5g-gateway]")
 	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile)
 
-	t := initRAN()
+	log.Printf("*** INIT RAN ONLY ***")
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	initRAN()
 
-	sigCh := make(chan os.Signal, 1)
+//	ctx, cancel := context.WithCancel(context.Background())
+//	defer cancel()
+
+//	sigCh := make(chan os.Signal, 1)
 	// signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM, syscall.SIGCONT)
-	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+//	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
 
-	log.Printf("run user plane")
-	t.updateNGAP("gnb.json")
-	t.initUE()
+//	log.Printf("run user plane")
+//	t.updateNGAP("gnb.json")
+//	t.initUE()
 
     // register all UEs
-	gnb := t.gnb
-	for _, c := range gnb.Camper {
-		ue := c.UE
-		t.registerUE(ue)
-	}
+//	gnb := t.gnb
+//	for _, c := range gnb.Camper {
+//		ue := c.UE
+//		t.registerUE(ue)
+//	}
 
-	for _, c := range gnb.Camper {
-		ue := c.UE
-		t.establishPDUSession(ue)
-	}
-	log.Printf("before user plane setup")
+//	for _, c := range gnb.Camper {
+//		ue := c.UE
+//		t.establishPDUSession(ue)
+//	}
+//	log.Printf("before user plane setup")
 
-	fatalCh := make(chan error, 1)
+//	fatalCh := make(chan error, 1)
 
-	for _, c := range gnb.Camper {
-		go func() {
-			if err := setupUserPlane(t, ctx,c); err != nil {
-				fatalCh <- err
-			}
-		}()
-	}
+//	for _, c := range gnb.Camper {
+//		go func() {
+//			if err := setupUserPlane(t, ctx,c); err != nil {
+//				fatalCh <- err
+//			}
+//		}()
+//	}
 
-	for {
-		select {
-		case err := <-fatalCh:
-			log.Printf("FATAL: %s", err)
-			return
-		}
-	}
+//	for {
+//		select {
+//		case err := <-fatalCh:
+//			log.Printf("FATAL: %s", err)
+//			return
+//		}
+//	}
 
 	return
 }
